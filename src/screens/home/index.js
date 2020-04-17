2404b4e042a6aa17eb99b3b3c9ba45d1c95afd3b
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-04-17 15:17:34 +0100

    feat<parcial>: home body

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import {
   FiGithub,
@@ -13,10 +13,21 @@ import {
   FiLayers,
   FiSettings,
   FiLogOut,
+  FiSearch,
+  FiGitBranch,
 } from 'react-icons/fi';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function Home() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/feMoraes0/repos?per_page=100').then((response) => {
+      setRepositories([...repositories, ...response.data]);
+    });
+  }, []);
+
   return (
     <div className='home-container'>
       <div className='app-menu'>
@@ -120,7 +131,64 @@ function Home() {
           </Link>
         </ul>
       </div>
-      <div className='body' />
+      <div className='body'>
+        <form>
+          <FiSearch size={20} />
+          <input type='text' placeholder='Search repository' />
+        </form>
+        <div className='resume'>
+          <div className='resume-box'>
+            <h6>Commits</h6>
+            <h3>100</h3>
+          </div>
+          <div className='resume-box'>
+            <h6>Pull Requests</h6>
+            <h3>100</h3>
+          </div>
+          <div className='resume-box'>
+            <h6>Repositories</h6>
+            <h3>100</h3>
+          </div>
+          <div className='resume-box'>
+            <h6>Stars</h6>
+            <h3>100</h3>
+          </div>
+          <div className='resume-box'>
+            <h6>Followers</h6>
+            <h3>100</h3>
+          </div>
+          <div className='resume-box'>
+            <h6>Following</h6>
+            <h3>100</h3>
+          </div>
+        </div>
+        <h6 className='title'>Repositories</h6>
+        <div className='repositories'>
+          {
+            repositories.map((repository) => (
+              <div className='repository-box'>
+                <div>
+                  <h5 className='title'>{repository.name}</h5>
+                  <h6 className='description'>{repository.language}</h6>
+                  <h6 className='description'>{repository.description}</h6>
+                </div>
+                <div className='repository-footer'>
+                  <h6>
+                    <FiStar />
+                    {' '}
+                    {`${repository.stargazers_count} stars`}
+                  </h6>
+                  <h6>
+                    <FiGitBranch />
+                    {' '}
+                    {`${repository.forks_count} forks`}
+                  </h6>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
